dfe9f4dafac2081bce0fb6d72e5cb55ff93d6705
Aratchaporn Jiamwijitkul <praearatchaporn@gmail.com>
2023-05-22 15:22:16 +0700

    Complete functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  let numbers = [];
+  for (let i = 1; i <= 100; i++) {
+    numbers.push(i);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>FizzBuzz</h1>
+      <p>
+        Write a program that prints the numbers from 1 to 100. But for multiples
+        of three print “Fizz” instead of the number and for the multiples of
+        five print “Buzz”. For numbers which are multiples of both three and
+        five print “FizzBuzz”.
+      </p>
+      <h3>Result :</h3>
+      <div className="Result">
+        {numbers.map((number, index) => {
+          let value = index + 1;
+          if (value % 3 === 0 && value % 5 !== 0) {
+            return (
+              <p className="Number" key={index}>
+                Fizz
+              </p>
+            );
+          } else if (value % 5 === 0 && value % 3 !== 0) {
+            return (
+              <p className="Number" key={index}>
+                Buzz
+              </p>
+            );
+          } else if (value % 3 === 0 && value % 5 === 0) {
+            return (
+              <p className="Number" key={index}>
+                FizzBuzz
+              </p>
+            );
+          } else {
+            return (
+              <p className="Number" key={index}>
+                {number}
+              </p>
+            );
+          }
+        })}
+      </div>
     </div>
   );
 }
